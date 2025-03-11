3cea8faf1a2c668e2ac999e21294164b70494689
TDAR1804 <tomasdeandreis@gmail.com>
2025-03-11 21:14:34 +0000

    pruebas unitest

--- Tests/test_parser.py
+++ Tests/test_parser.py
@@ -1,6 +1,6 @@
 import pytest
 import datetime
-from Parser.parser import extract_data, save_to_s3, DESTINATION_BUCKET
+from parser.parser import extract_data, save_to_s3, DESTINATION_BUCKET
 import boto3
 from moto import mock_s3
 
